791cbbdead793ebb68b9298f27c004d7feba8fa2
Octavia Thomson <octaviathomson@gmail.com>
2020-11-13 12:25:40 +0000

    cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   root to: "cocktails#index"
+#   resources :cocktails do
+#     resources :doses, only: [:new, :create]
+#   end
+#   resources :doses, only: [:destroy]
+# end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'doses/new'
   root to: "cocktails#index"
-  resources :cocktails, only: [:index, :show, :new, :create]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: :destroy
 end
